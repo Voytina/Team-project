783238fbac7ed6651e0dc9b9c76aca46530bb1b7
Voytina <voytinakolya@gmail.com>
2023-11-01 12:38:58 +0200

    button

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-
 const mobMenu = document.querySelector('.mob-menu')
 const btnOpen = document.querySelector('.menu-open-btn')
 const btnClose = document.querySelector('.menu-close-btn')
 
- 
 btnOpen.addEventListener('click', () => {
     mobMenu.classList.add('is-open')
 })
 btnClose.addEventListener('click', () => {
     mobMenu.classList.remove('is-open')
+})
+
+const menuLink = document.querySelectorAll('.menu-link')
+menuLink.forEach((elem) => {
+    elem.addEventListener('click', () => {
+        mobMenu.classList.remove('is-open')
+    })
 })
